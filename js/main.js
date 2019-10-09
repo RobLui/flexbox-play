c46774af1cd5c898228e9a2cb5b916781db9c76e
Robbert <robbertluit@hotmail.com>
2019-10-09 22:52:18 +0200

    playing with values n colors

--- js/main.js
+++ js/main.js
@@ -10,24 +10,34 @@ $(document).ready(function (e) {
             var item = $(this);
             setTimeout(function (e) {
                 counter++;
-
                 item.addClass('active');
-                if (counter < 18) {
-                    item.addClass('flex-2');
+                if (counter <= 10) {
+                    item.removeClass('bg-orange');
+                    item.removeClass('bg-blue');
+                    item.removeClass('bg-yellow');
+                    item.addClass('bg-blue');
                 }
-                if (counter > 18) {
+                if (counter < 19 && counter >= 10) {
+                    item.addClass('flex-2');
+                    item.removeClass('bg-orange');
+                    item.removeClass('bg-blue');
+                    item.removeClass('bg-yellow');
+                    item.addClass('bg-orange');
+                }
+                if (counter < 28 && counter >= 19) {
                     item.addClass('flex-3');
+                    item.removeClass('bg-orange');
+                    item.removeClass('bg-blue');
+                    item.removeClass('bg-yellow');
+                    item.addClass('bg-yellow');
                 }
             }, time);
             time += basetime;
             setTimeout(function (e) {
                 item.removeClass('active');
-                if (counter <= 18) {
-                    item.removeClass('flex-2');
-                }
-                if (counter >= 18) {
-                    item.removeClass('flex-3');
-                }
+                item.removeClass('flex-2');
+                item.removeClass('flex-3');
+
             }, time + basetime);
         });
     }
