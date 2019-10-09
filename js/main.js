1b9b0fe2f1dfe82e9fa74087cc7862d5b9355fd5
Robbert <robbertluit@hotmail.com>
2019-10-09 22:29:48 +0200

    fun animations with js added classes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function (e) {
     const basetime = 500;
     var time = 0;
+    let counter = 0;
 
     // iterate 3 x 9
     for (var index = 0; index < 3; index++) {
@@ -8,11 +9,25 @@ $(document).ready(function (e) {
         $('.flex-1').each(function (i) {
             var item = $(this);
             setTimeout(function (e) {
+                counter++;
+
                 item.addClass('active');
+                if (counter < 18) {
+                    item.addClass('flex-2');
+                }
+                if (counter > 18) {
+                    item.addClass('flex-3');
+                }
             }, time);
             time += basetime;
             setTimeout(function (e) {
                 item.removeClass('active');
+                if (counter <= 18) {
+                    item.removeClass('flex-2');
+                }
+                if (counter >= 18) {
+                    item.removeClass('flex-3');
+                }
             }, time + basetime);
         });
     }
